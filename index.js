48b58fee03d0fe41bedf19f92f622bc5d04680d4
beginal <beginal01@gmail.com>
2020-03-10 16:52:29 +0900

    change

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const express = require('express');
+const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+const config = require('./config/key')
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://beginal:code.009@youtubeclone-wyrzm.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected'))
 .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('안녕하세요!'))
+app.get('/', (req, res) => res.send('안녕하세요! 하이'))
+
+app.post('/register', (req,res) => {
+
+const user = new User(req.body)
+
+user.save((err, userInfo) => {
+  if(err) return res.json({ success: false, err })
+  return res.status(200).json({
+    success: true
+  })
+})
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
