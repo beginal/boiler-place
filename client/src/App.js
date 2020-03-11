61a1151fad80170f6139198262abe4025e53528c
beginal <beginal01@gmail.com>
2020-03-11 21:51:52 +0900

    0311

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import LandingPage from "./Components/views/LandingPage/LandingPage"
+import LoginPage from "./Components/views/LoginPage/LoginPage"
+import RegisterPage from "./Components/views/RegisterPage/RegisterPage"
+function App() {
+  return (   
+      <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/Login" component={LoginPage} />
+          <Route exact path="/Register" component={RegisterPage} />
+          
+        </Switch>
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
